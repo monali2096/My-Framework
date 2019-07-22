78cd571996cfcdb9a8256f94449345a1a8f04df4
monali2096 <monalidalvi336@gmail.com>
2019-07-22 12:11:50 +0530

    name change

--- config/env/development.js
+++ config/env/development.js
@@ -1,6 +1,6 @@
 export default {
     port: 3000,
     mongodbUrl:
-        "mongodb://localhost:27017/wohligFramework?readPreference=primary",
+        "mongodb://localhost:27017/MyFrameworkTraning?readPreference=primary",
     url: "http://wohlig.io"
 }
